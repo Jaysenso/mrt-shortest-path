b1d0126bb952d348100db9c3638914e7174edc5e
Jaysenso <jsenongxuan@gmail.com>
2024-04-09 13:09:02 +0800

    Implemented Instruction Dialog Box

---
+++ src/assets/components/DialogBox.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export default function DialogBox() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <React.Fragment>
+            <Button variant="contained" onClick={handleClickOpen}>
+                Instructions
+            </Button>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title" textAlign={"center"}>
+                    {"Instruction"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Please select your starting and ending stations. If you
+                        need to change your selection, simply click on the
+                        station again to deselect it
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} autoFocus>
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </React.Fragment>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,27 @@
 import MrtMap from "../assets/Static/MrtMap";
 import StationButton from "../assets/components/StationButton";
 import Stations from "../assets/Static/Stations_pos";
+import DialogBox from "../assets/components/DialogBox";
 const Home = () => {
     return (
-        <div style={{ position: "relative", width: "100%", height: "100%" }}>
-            <MrtMap />
-            <StationButton stations={Stations} />
-        </div>
+        <>
+            <div
+                style={{
+                    position: "fixed",
+                    right: 10,
+                    bottom: 10,
+                    zIndex: 1000,
+                }}
+            >
+                <DialogBox />
+            </div>
+            <div
+                style={{ position: "relative", width: "100%", height: "100%" }}
+            >
+                <MrtMap />
+                <StationButton stations={Stations} />
+            </div>
+        </>
     );
 };
 
